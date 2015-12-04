e9514c8498c7e68b8fc293450abb028028172ba8
lcc <18260445451@163.com>
2015-12-04 14:14:16 +0800

    新工程的添加

---
+++ TheFirstGitTest/TheFirstGitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheFirstGitTest
+//
+//  Created by lcc on 15/12/4.
+//  Copyright © 2015年 lcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
